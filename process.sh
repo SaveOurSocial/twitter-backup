2beeb83a549328433a8d8884e42c275eec3a99d0
Eli Finer <eli.finer@gmail.com>
2022-02-28 10:20:09 -0800

    upload to S3 bucket

--- process.sh
+++ process.sh
@@ -3,7 +3,15 @@ if [ $1 == "" ]; then
     echo "usage $0 <username>"
     exit 1
 fi
+
 node followers.js $1
 node make-csv.js $1-followers.txt
-curl --http1.1 --progress-bar --upload-file $1-followers.csv https://transfer.sh | cat
+
+FILEPATH=$1-followers.csv
+ID=`uuidgen | md5 | head -c10`
+BASENAME=`basename $FILEPATH`
+
+aws s3 cp $FILEPATH s3://saveoursocial-production/$ID/$BASENAME
+
 echo
+echo "https://backups.saveoursocial.co/$ID/$BASENAME"
