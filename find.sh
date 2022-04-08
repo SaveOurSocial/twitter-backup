b56ece5da61a0f350c7999ce2e8eeee00e5b9a4b
Eli Finer <eli.finer@gmail.com>
2022-04-08 17:11:43 -0400

    improve find.sh to show only csv files

--- find.sh
+++ find.sh
@@ -4,5 +4,5 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 NAME=$1
-aws s3 ls --recursive s3://saveoursocial-production/ | grep $NAME | awk '{print $4}' | \
+aws s3 ls --recursive s3://saveoursocial-production/ | grep $NAME-followers.csv | awk '{print $4}' | \
     while read line; do echo "https://backups.saveoursocial.co/$line"; done
