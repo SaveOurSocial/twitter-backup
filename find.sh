2682e209ca578d7d40d13367407dd39ef246b27b
Eli Finer <eli.finer@gmail.com>
2022-04-14 21:37:35 -0700

    show dates in find.sh results

--- find.sh
+++ find.sh
@@ -4,5 +4,6 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 NAME=$1
-aws s3 ls --recursive s3://saveoursocial-production/ | grep $NAME-followers.csv | awk '{print $4}' | \
-    while read line; do echo "https://backups.saveoursocial.co/$line"; done
+aws s3 ls --recursive s3://saveoursocial-production/ |\
+    grep $NAME-followers.csv |\
+    awk '{print $1 " " $2 " https://backups.saveoursocial.co/"$4}'
